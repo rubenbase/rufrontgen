a1fb98a28c6b3252c347ae8e9019ad4a6fd5697a
rubenbase <daggosso@hotmail.com>
2018-06-19 08:53:00 +0200

    Added Register UI

---
+++ packages/client/src/apollo.ts
@@ -0,0 +1,12 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://localhost:4000",
+    credentials: "include"
+  }),
+
+  cache: new InMemoryCache()
+});
---
+++ packages/client/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { RegisterView } from "./ui/RegisterView";
+
+export const RegisterConnector = () => <RegisterView />;
