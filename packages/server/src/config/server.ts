9c036164a7ddf04c07c335b836f40b4d245a64b5
rubenbase <daggosso@hotmail.com>
2018-06-20 12:55:05 +0200

    Added yup validation + fixed server types problems

--- packages/server/src/config/server.ts
+++ packages/server/src/config/server.ts
@@ -27,13 +27,13 @@ if (redisDebugMode) {
 // Creates the GraphQL server
 const server = new GraphQLServer({
   schema: genSchema(),
-  context: ({ request }) => ({
+  context: ({ request }: any) => ({
     redis,
     url: request.protocol + "://" + request.get("host"),
     session: request.session,
     req: request
   })
-});
+} as any);
 
 module.exports.server = server;
 module.exports.redis = redis;
