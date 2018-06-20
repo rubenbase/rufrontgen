5cef4779425c01787bed1455fecb31ccc5fa9b8c
rubenbase <daggosso@hotmail.com>
2018-06-20 18:49:51 +0200

    Added and configure common folder to share code between projects with yarn workspaces

---
+++ packages/common/dist/yupSchemas/user.d.ts
@@ -0,0 +1,6 @@
+import * as yup from "yup";
+export declare const emailNotLongEnough = "email must be at least 3 characters";
+export declare const invalidEmail = "email must be a valid email";
+export declare const passwordNotLongEnough = "password must be at least 3 characters";
+export declare const registerPasswordValidation: yup.StringSchema;
+export declare const validUserSchema: yup.ObjectSchema<{}>;
---
+++ packages/common/dist/yupSchemas/user.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const yup = require("yup");
+exports.emailNotLongEnough = "email must be at least 3 characters";
+exports.invalidEmail = "email must be a valid email";
+exports.passwordNotLongEnough = "password must be at least 3 characters";
+exports.registerPasswordValidation = yup
+    .string()
+    .min(3, exports.passwordNotLongEnough)
+    .max(255)
+    .required();
+exports.validUserSchema = yup.object().shape({
+    email: yup
+        .string()
+        .required()
+        .min(3, exports.emailNotLongEnough)
+        .max(255)
+        .email(exports.invalidEmail),
+    password: exports.registerPasswordValidation
+});
+//# sourceMappingURL=user.js.map
