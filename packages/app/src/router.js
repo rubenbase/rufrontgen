8d0266adb234ef3fa501318b72f76d2485a03b37
rubenbase <daggosso@hotmail.com>
2018-07-16 13:24:07 +0200

    Added styled menu

--- packages/app/src/router.js
+++ packages/app/src/router.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import {
   createStackNavigator,
   createBottomTabNavigator,
@@ -8,6 +10,7 @@ import RegisterConnector from "./modules/register/RegisterConnector";
 import LoginConnector from "./modules/login/LoginConnector";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export const SignedOut = createStackNavigator({
   SignUp: {
@@ -24,20 +27,42 @@ export const SignedOut = createStackNavigator({
   }
 });
 
-export const SignedIn = createBottomTabNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      tabBarLabel: "Home"
+export const SignedIn = createBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: "EXPLORE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-search-outline" color={tintColor} size={24} />
+        )
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: "PROFILE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-person-outline" color={tintColor} size={24} />
+        )
+      }
     }
   },
-  Profile: {
-    screen: Profile,
-    navigationOptions: {
-      tabBarLabel: "Profile"
+  {
+    tabBarOptions: {
+      activeTintColor: "red",
+      inactiveTintColor: "grey",
+      style: {
+        backgroundColor: "white",
+        borderTopWidth: 0,
+        shadowOffset: { width: 5, height: 3 },
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        elevation: 5
+      }
     }
   }
-});
+);
 
 export const createRootNavigator = (signedIn = false) => {
   return createSwitchNavigator(
--- packages/app/src/screens/Home.js
+++ packages/app/src/screens/Home.js
@@ -1,35 +1,21 @@
 import React from "react";
-import { ScrollView, Text, Linking, View } from "react-native";
+import { ScrollView, Text, StyleSheet, View } from "react-native";
 
-const images = [
-  {
-    key: 1,
-    name: "Nathan Anderson",
-    url: "https://unsplash.com/photos/C9t94JC4_L8"
-  },
-  {
-    key: 2,
-    name: "Jamison McAndie",
-    url: "https://unsplash.com/photos/waZEHLRP98s"
-  },
-  {
-    key: 3,
-    name: "Alberto Restifo",
-    url: "https://unsplash.com/photos/cFplR9ZGnAk"
-  },
-  {
-    key: 4,
-    name: "John Towner",
-    url: "https://unsplash.com/photos/89PFnHKg8HE"
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Profile</Text>
+      </View>
+    );
   }
-];
+}
 
-export default () => (
-  <View style={{ flex: 1 }}>
-    <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
-      {images.map(({ name, url, key }) => (
-        <Text style={{ marginBottom: 10 }}>Photo by {name}.</Text>
-      ))}
-    </ScrollView>
-  </View>
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
