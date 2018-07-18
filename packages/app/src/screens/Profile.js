45f3d512609f42a43fc586bbd88091d868c39fac
rubenbase <daggosso@hotmail.com>
2018-07-18 13:28:44 +0200

    Finished UI for Profile view

--- packages/app/src/screens/Profile.js
+++ packages/app/src/screens/Profile.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 import {
+  StyleSheet,
   View,
   Text,
-  StyleSheet,
-  SafeAreaView,
-  TextInput,
   Platform,
-  StatusBar
+  StatusBar,
+  SafeAreaView
 } from "react-native";
-import { Avatar } from "react-native-elements";
+
+import { ButtonGroup } from "react-native-elements";
 import Icon from "react-native-vector-icons/Ionicons";
 
 class Explore extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedIndex: 2
+    };
+    this.updateIndex = this.updateIndex.bind(this);
+  }
+
+  updateIndex(selectedIndex) {
+    this.setState({ selectedIndex });
+  }
   componentWillMount() {
     this.startHeaderHeight = 80;
     if (Platform.OS == "android") {
@@ -20,36 +31,76 @@ class Explore extends Component {
   }
 
   render() {
+    const buttons = ["Hoy", "Semana", "Mes"];
+    const { selectedIndex } = this.state;
+
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1 }}>
+        <View style={styles.Container}>
+          <View style={styles.Content}>
+            <Text style={styles.ImagePlaceholder}>Placeholder </Text>
+            <View style={styles.Promo}>
+              <View style={styles.PromoTextContainer}>
+                <Text style={styles.PromoText}>Space for promotion</Text>
+                <Text style={[styles.PromoText, styles.TextBold]}>
+                  websiteofpromo.com
+                </Text>
+              </View>
+            </View>
+          </View>
           <View
             style={{
-              height: this.startHeaderHeight,
+              flex: 1,
+              paddingHorizontal: 20,
               backgroundColor: "white",
-              borderBottomWidth: 1,
-              borderBottomColor: "#dddddd"
+              paddingTop: 30
             }}
           >
             <View
               style={{
-                flexDirection: "row",
-                padding: 10,
-                backgroundColor: "white",
-                marginHorizontal: 0,
-                shadowOffset: { width: 0, height: 0 },
-                shadowColor: "black",
-                shadowOpacity: 0.2,
-                marginTop: Platform.OS == "android" ? 30 : null
+                flexDirection: "row"
               }}
             >
-              <Avatar
-                large
-                rounded
-                title="BP"
-                onPress={() => console.log("Works!")}
-                activeOpacity={0.7}
-              />
+              <View>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    marginRight: 10
+                  }}
+                >
+                  Hello,
+                  <Text
+                    style={{
+                      fontSize: 24,
+                      fontWeight: "700"
+                    }}
+                  >
+                    {" "}
+                    Ruben Costa
+                  </Text>
+                </Text>
+              </View>
+              <View style={styles.ButtonContainer}>
+                <View style={[styles.Button, styles.UtilityButton]}>
+                  <Text style={[styles.ButtonText, styles.UtilityButtonText]}>
+                    Edit
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                marginTop: 20
+              }}
+            >
+              <View>
+                <ButtonGroup
+                  onPress={this.updateIndex}
+                  selectedIndex={selectedIndex}
+                  buttons={buttons}
+                  containerStyle={{ height: 50 }}
+                />
+              </View>
             </View>
           </View>
         </View>
@@ -57,4 +108,63 @@ class Explore extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    alignSelf: "stretch"
+  },
+  Button: {
+    flexDirection: "row",
+    padding: 8,
+    borderRadius: 6,
+    justifyContent: "space-between",
+    width: 60
+  },
+  ButtonText: {
+    color: "white"
+  },
+  PromoTextContainer: {
+    flex: 1,
+    alignItems: "flex-end"
+  },
+
+  Content: {
+    height: 300,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    marginTop: Platform.OS == "android" ? 30 : null
+  },
+  ImagePlaceholder: {
+    backgroundColor: "#1e1e1e",
+    color: "white",
+    padding: 15,
+    fontWeight: "bold",
+    flex: 1,
+    alignItems: "flex-end",
+    fontSize: 20,
+    alignSelf: "stretch",
+    borderRadius: 6
+  },
+  Promo: {
+    height: 60,
+    flexDirection: "row",
+    paddingTop: 16,
+    paddingRight: 8,
+    paddingBottom: 16,
+    paddingLeft: 8
+  },
+  UtilityButton: {
+    backgroundColor: "#ECECEC",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  UtilityButtonText: {
+    color: "black"
+  },
+  TextBold: {
+    fontWeight: "bold"
+  }
+});
 export default Explore;
