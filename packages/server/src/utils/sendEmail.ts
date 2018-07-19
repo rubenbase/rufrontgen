626de3d500f37b5b276d61366b15aa981217b2ce
rubenbase <daggosso@hotmail.com>
2018-07-20 00:02:29 +0200

    Implemented own email solution with Ethereal and Nodemailer as SparkPost limit was reached.

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,22 +1,39 @@
+// Use at least Nodemailer v4.1.0
+import nodemailer = require("nodemailer");
 import SparkPost = require("sparkpost");
 const client = new SparkPost(process.env.SPARKPOST_API_KEY);
 
 export const sendEmail = async (recipient: string, url: string) => {
-  const response = await client.transmissions.send({
-    options: {
-      sandbox: true
-    },
-    content: {
-      from: "testing@sparkpostbox.com",
-      subject: "Confirm Email!",
-      html: `<html>
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "ze7fym7yn6th42g2@ethereal.email",
+      pass: "8XRyggHfGtC1xfsPJv"
+    }
+  });
+
+  // Message object
+  const message = {
+    from: "Sender Name <sender@example.com>",
+    to: `Recipient <${recipient}>`,
+    subect: "Nodemailer is unicode friendly ✔",
+    text: "Hello to myself!",
+    html: `<html>
         <body>
         <p>Testing SparkPost - the world's most awesomest email service!</p>
         <a href="${url}">Confirm email</a>
         </body>
         </html>`
-    },
-    recipients: [{ address: recipient }]
+  };
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log("Error occurred. " + err.message);
+    }
+
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   });
-  console.log(response);
 };
