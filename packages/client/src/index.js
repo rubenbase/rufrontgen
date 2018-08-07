d8e21ff06f725b2c16861d0534d24a9fafcbad27
rubenbase <daggosso@hotmail.com>
2018-08-07 17:17:41 +0200

    Added styles structure, antd overridings, vendors, core, and cleanui

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -5,7 +5,14 @@ import { ApolloProvider } from "react-apollo";
 import registerServiceWorker from "./registerServiceWorker";
 import { client } from "./apollo";
 import { Routes } from "./routes";
-import "./index.css";
+
+import 'resources/_antd.less' // redefinition AntDesign variables
+import 'bootstrap/dist/css/bootstrap.min.css' // bootstrap styles
+
+import 'resources/AntStyles/AntDesign/antd.cleanui.scss'
+import 'resources/CleanStyles/Core/core.cleanui.scss'
+import 'resources/CleanStyles/Vendors/vendors.cleanui.scss'
+
 
 ReactDOM.render(
   <ApolloProvider client={client}>
