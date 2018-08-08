aecc7d6c91ca6a59f5303eaba49f7ee250f2ef2f
Marcos Muñiz García <esechinako@gmail.com>
2018-08-08 15:35:02 +0200

    add home contant and styles

--- packages/client/src/modules/home/ui/HomeView.js
+++ packages/client/src/modules/home/ui/HomeView.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Menu, Icon } from "antd";
+import LiveSearch from "./LiveSearch";
 import "./style.scss";
 
 const SubMenu = Menu.SubMenu;
@@ -8,27 +9,33 @@ export class HomeView extends React.PureComponent {
   render() {
     return (
       <div>
-        <Menu onClick={this.handleClick} mode="horizontal">
-          <Menu.Item key="home">
-            <Icon type="home" />Inicio
-          </Menu.Item>
-          <Menu.Item key="mail">
-            <Icon type="mail" />Contacto
-          </Menu.Item>
-          <Menu.Item key="user">
-            <Icon type="user" />Iniciar sesión
-          </Menu.Item>
-          <SubMenu
-            title={
-              <span>
-                <Icon type="user" />Iniciar sesión
-              </span>
-            }
-          >
-            <Menu.Item key="setting:1">Log in</Menu.Item>
-            <Menu.Item key="setting:2">Register</Menu.Item>
-          </SubMenu>
-        </Menu>
+        <div className="topbar">
+          <div className="topbar__left">
+            <a href="javascript: void(0);">
+              <img src="../../../../public/img/logo.svg" alt="Yoowriter logo" />
+            </a>
+          </div>
+          <div className="topbar__right">
+            <Menu onClick={this.handleClick} mode="horizontal">
+              <Menu.Item key="trophy">
+                <Icon type="trophy" />Premios abiertos
+              </Menu.Item>
+              <Menu.Item key="home">
+                <LiveSearch />
+              </Menu.Item>
+              <SubMenu
+                title={
+                  <span>
+                    <Icon type="user" />Iniciar sesión
+                  </span>
+                }
+              >
+                <Menu.Item key="setting:1">Entrar</Menu.Item>
+                <Menu.Item key="setting:2">Registrarse</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </div>
+        </div>
         <div class="jumbotron">
           <h1 class="display-4">¡Bienvenidos a Yoowriter!</h1>
           <p class="lead">
@@ -45,18 +52,93 @@ export class HomeView extends React.PureComponent {
         </div>
         <div class="container">
           <div class="row">
-            <div class="card">
-              <img class="card-img-top" src="" alt="" title="" />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text myclass">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
+            <div class="col-sm">
+              <div class="card">
+                <img class="card-img-top" src="" alt="" title="" />
+                <div class="card-body">
+                  <h5 class="card-title">¿Eres una entidad convocante?</h5>
+                  <ul>
+                    <li>Inscríbete en Yoowriter</li>
+                    <li>Registrar un premio literario</li>
+                    <li>Convoca ya n premio literario</li>
+                    <li>Consulta el estado de un concurso</li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">
+                    Regístrate en Yoowriter
+                  </a>
+                </div>
               </div>
+            </div>
+            <div class="col-sm">
+              <div class="card">
+                <img class="card-img-top" src="" alt="" title="" />
+                <div class="card-body">
+                  <h5 class="card-title">¿Eres un autor?</h5>
+                  <ul>
+                    <li>Inscríbete como autor en Yoowriter</li>
+                    <li>Utiliza servicios avanzados</li>
+                    <li>Presenta una obra a un concurso</li>
+                    <li>Consulta el estado de tus obras</li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">
+                    Regístrate en Yoowriter
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm">
+              <div class="card">
+                <img class="card-img-top" src="" alt="" title="" />
+                <div class="card-body">
+                  <h5 class="card-title">
+                    ¿Eres una persona atenta al mundo literario?
+                  </h5>
+                  <ul>
+                    <li>Inscríbete en Yoowriter</li>
+                    <li>Conoce la actualidad de los premios literarios</li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">
+                    Regístrate en Yoowriter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <h2>Ventajas de Yoowriter</h2>
+          <div class="row">
+            <div class="col-sm">
+              <Icon type="lock" />
+              <h5>Confidencialidad</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit, vivamus
+                non cum dictumst luctus condimentum ullamcorper
+              </p>
+            </div>
+            <div class="col-sm">
+              <Icon type="sync" />
+              <h5>Transparencia</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit, vivamus
+                non cum dictumst luctus condimentum ullamcorper
+              </p>
+            </div>
+            <div class="col-sm">
+              <Icon type="hourglass" />
+              <h5>Eficiencia</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit, vivamus
+                non cum dictumst luctus condimentum ullamcorper
+              </p>
+            </div>
+            <div class="col-sm">
+              <Icon type="heart" />
+              <h5>Sostenibilidad</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit, vivamus
+                non cum dictumst luctus condimentum ullamcorper
+              </p>
             </div>
           </div>
         </div>
