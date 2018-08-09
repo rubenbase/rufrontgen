4975eb9fd7712b052000c1aeb1e74038dc3d7246
rubenbase <daggosso@hotmail.com>
2018-08-09 14:12:30 +0200

    Updated antd, finished email stuff, rm apollo codegen

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -19,8 +19,104 @@ export const sendEmail = async (recipient: string, url: string) => {
     text: "Hello to myself!",
     html: `<html>
         <body>
-        <p>Testing SparkPost - the world's most awesomest email service!</p>
-        <a href="${url}">Confirm email</a>
+        <div
+                  width="100%"
+                  style={{ background: '#eceff4', padding: '50px 20px', color: '#514d6a' }}
+                >
+                  <div style={{ maxWidth: '700px', margin: '0px auto', fontSize: '14px' }}>
+                    <table
+                      cellPadding="0"
+                      cellSpacing="0"
+                      style={{ width: '100%', marginBottom: '20px', border: '0px' }}
+                    >
+                      <tbody>
+                        <tr>
+                          <td style={{ verticalAlign: 'top' }}>
+                            <img
+                              src="resources/images/logo.png"
+                              alt="Clean UI Admin Template"
+                              style={{ height: '40px' }}
+                            />
+                          </td>
+                          <td style={{ textAlign: 'right', verticalAlign: 'middle' }}>
+                            <span style={{ color: '#a09bb9' }}>Some Description</span>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div style={{ padding: '40px 40px 20px 40px', background: '#fff' }}>
+                      <table
+                        cellPadding="0"
+                        cellSpacing="0"
+                        style={{ width: '100%', border: '0px' }}
+                      >
+                        <tbody>
+                          <tr>
+                            <td>
+                              <h5
+                                style={{
+                                  marginBottom: '20px',
+                                  color: '#24222f',
+                                  fontWeight: '600',
+                                }}
+                              >
+                                Password Reset
+                              </h5>
+                              <p>
+                                Seems like you forgot your password for Clean UI. If this is true,
+                                click below to reset your password.
+                              </p>
+                              <div style={{ textAlign: 'center' }}>
+                                <a
+                                  href="${url}"
+                                  style={{
+                                    display: 'inline-block',
+                                    padding: '11px 30px 6px',
+                                    margin: '20px 0px 30px',
+                                    fontSize: '15px',
+                                    color: '#fff',
+                                    background: '#01a8fe',
+                                    borderRadius: '5px',
+                                  }}
+                                >
+                                  Reset Password
+                                </a>
+                              </div>
+                              <p>
+                                If you did not forgot your password you can safely ignore his email.
+                              </p>
+                              <p>
+                                Regards,<br />Mediatec Software
+                              </p>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div
+                      style={{
+                        textAlign: 'center',
+                        fontSize: '12px',
+                        color: '#a09bb9',
+                        marginTop: '20px',
+                      }}
+                    >
+                      <p>
+                        Mediatec Software Inc., Abbey Road, San Francisco CA 94102
+                        <br />
+                        Don't like these emails?{' '}
+                        <a
+                          href="javascript: void(0);"
+                          style={{ color: '#a09bb9', textDecoration: 'underline' }}
+                        >
+                          Unsubscribe
+                        </a>
+                        <br />
+                        Powered by Clean UI
+                      </p>
+                    </div>
+                  </div>
+                </div>
         </body>
         </html>`
   };
