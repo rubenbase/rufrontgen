c38b0727378453ed79ffb29b84a2d1bb0538e9b7
Marcos Muñiz García <esechinako@gmail.com>
2018-08-10 13:44:30 +0200

    change class for className and working on menu

--- packages/client/src/modules/home/ui/HomeView.js
+++ packages/client/src/modules/home/ui/HomeView.js
@@ -9,66 +9,66 @@ export class HomeView extends React.PureComponent {
   render() {
     return (
       <div>
-        <div className="menuTop">
-          <div className="menuTop__logo">
-            <div className="menuTop__logoContainer">
-              <img src="resources/images/logo.svg" alt="" />
+        <Menu className="topbar" onClick={this.handleClick} mode="horizontal">
+          <div className="topbar__left">
+            <div className="menuTop__logo">
+              <div className="menuTop__logoContainer">
+                <img src="resources/images/logo-white.svg" alt="" />
+              </div>
             </div>
           </div>
-          <Menu onClick={this.handleClick} mode="horizontal">
-            <Menu className="topbar__right">
-              <Menu.Item key="trophy">
-                <Icon type="trophy" />Premios abiertos
-              </Menu.Item>
-              <Menu.Item key="home">
-                <LiveSearch />
-              </Menu.Item>
-              <SubMenu
-                title={
-                  <span>
-                    <Icon type="user" />Iniciar sesión
-                  </span>
-                }
-              >
-                <Menu.Item key="setting:1">Entrar</Menu.Item>
-                <Menu.Item key="setting:2">Registrarse</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Menu>
-        </div>
+
+          <Menu.Item key="trophy">
+            <Icon type="trophy" />Premios abiertos
+          </Menu.Item>
+          <Menu.Item key="home">
+            <LiveSearch />
+          </Menu.Item>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="user" />Iniciar sesión
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">Entrar</Menu.Item>
+            <Menu.Item key="setting:2">Registrarse</Menu.Item>
+          </SubMenu>
+        </Menu>
+
         <div
-          class="jumbotron"
+          className="jumbotron"
           style={{ backgroundImage: `url(resources/images/bg.jpg)` }}
         >
-          <h1 class="display-4 text-center text-light">
+          <h1 className="display-4 text-center text-light">
             ¡Bienvenidos a Yoowriter!
           </h1>
-          <p class="lead text-center text-light">
+          <p className="lead text-center text-light">
             Yoowriter nace para hacer los Premios Literarios más transparentes,
             fiables y seguros.
           </p>
 
-          <p class="text-center text-light">
+          <p className="text-center text-light">
             Comprueba todo lo que te da Yoowriter.
           </p>
-          <p class="lead text-center">
-            <a class="btn btn-primary btn-lg" href="#" role="button">
+          <p className="lead text-center">
+            <a className="btn btn-primary btn-lg" href="#" role="button">
               Registrarme
             </a>
           </p>
         </div>
-        <div class="container">
-          <div class="row features">
-            <div class="col-sm">
-              <div class="card">
+        <div className="container">
+          <div className="row features">
+            <div className="col-sm">
+              <div className="card">
                 <img
-                  class="card-img-top"
+                  className="card-img-top"
                   src="resources/images/bg.jpg"
                   alt=""
                   title=""
                 />
-                <div class="card-body">
-                  <h5 class="card-title text-center">
+                <div className="card-body">
+                  <h5 className="card-title text-center">
                     ¿Eres una entidad convocante?
                   </h5>
                   <ul>
@@ -77,51 +77,51 @@ export class HomeView extends React.PureComponent {
                     <li>Convoca ya n premio literario</li>
                     <li>Consulta el estado de un concurso</li>
                   </ul>
-                  <a href="#" class="btn btn-primary btn-block">
+                  <a href="#" className="btn btn-primary btn-block">
                     Regístrate en Yoowriter
                   </a>
                 </div>
               </div>
             </div>
-            <div class="col-sm">
-              <div class="card">
+            <div className="col-sm">
+              <div className="card">
                 <img
-                  class="card-img-top"
+                  className="card-img-top"
                   src="resources/images/writer.jpg"
                   alt=""
                   title=""
                 />
-                <div class="card-body">
-                  <h5 class="card-title text-center">¿Eres un autor?</h5>
+                <div className="card-body">
+                  <h5 className="card-title text-center">¿Eres un autor?</h5>
                   <ul>
                     <li>Inscríbete como autor en Yoowriter</li>
                     <li>Utiliza servicios avanzados</li>
                     <li>Presenta una obra a un concurso</li>
                     <li>Consulta el estado de tus obras</li>
                   </ul>
-                  <a href="#" class="btn btn-primary btn-block">
+                  <a href="#" className="btn btn-primary btn-block">
                     Regístrate en Yoowriter
                   </a>
                 </div>
               </div>
             </div>
-            <div class="col-sm">
-              <div class="card">
+            <div className="col-sm">
+              <div className="card">
                 <img
-                  class="card-img-top"
+                  className="card-img-top"
                   src="resources/images/reader.jpg"
                   alt=""
                   title=""
                 />
-                <div class="card-body">
-                  <h5 class="card-title text-center">
+                <div className="card-body">
+                  <h5 className="card-title text-center">
                     ¿Eres una persona atenta al mundo literario?
                   </h5>
                   <ul>
                     <li>Inscríbete en Yoowriter</li>
                     <li>Conoce la actualidad de los premios literarios</li>
                   </ul>
-                  <a href="#" class="btn btn-primary btn-block">
+                  <a href="#" className="btn btn-primary btn-block">
                     Regístrate en Yoowriter
                   </a>
                 </div>
@@ -129,11 +129,11 @@ export class HomeView extends React.PureComponent {
             </div>
           </div>
         </div>
-        <div class="container">
-          <h2 class="text-center">Ventajas de Yoowriter</h2>
-          <div class="row features">
-            <div class="col-sm">
-              <h5 class="text-center">
+        <div className="container">
+          <h2 className="text-center">Ventajas de Yoowriter</h2>
+          <div className="row features">
+            <div className="col-sm">
+              <h5 className="text-center">
                 <Icon type="lock" /> Confidencialidad
               </h5>
               <p>
@@ -141,8 +141,8 @@ export class HomeView extends React.PureComponent {
                 non cum dictumst luctus condimentum ullamcorper
               </p>
             </div>
-            <div class="col-sm">
-              <h5 class="text-center">
+            <div className="col-sm">
+              <h5 className="text-center">
                 <Icon type="sync" /> Transparencia
               </h5>
               <p>
@@ -150,8 +150,8 @@ export class HomeView extends React.PureComponent {
                 non cum dictumst luctus condimentum ullamcorper
               </p>
             </div>
-            <div class="col-sm">
-              <h5 class="text-center">
+            <div className="col-sm">
+              <h5 className="text-center">
                 <Icon type="hourglass" /> Eficiencia
               </h5>
               <p>
@@ -159,8 +159,8 @@ export class HomeView extends React.PureComponent {
                 non cum dictumst luctus condimentum ullamcorper
               </p>
             </div>
-            <div class="col-sm">
-              <h5 class="text-center">
+            <div className="col-sm">
+              <h5 className="text-center">
                 <Icon type="heart" /> Sostenibilidad
               </h5>
               <p>
