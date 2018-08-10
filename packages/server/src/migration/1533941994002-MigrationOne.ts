27abc112cb8d3020444fdaf692d419af66e8a548
rubenbase <daggosso@hotmail.com>
2018-08-11 01:00:10 +0200

    setup first migration

---
+++ packages/server/src/migration/1533941994002-MigrationOne.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class MigrationOne1533941994002 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
