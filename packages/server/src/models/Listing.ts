f0f59a6c60c7dcf5f6012360e99fccaa17c05982
rubenbase <daggosso@hotmail.com>
2018-08-11 16:27:45 +0200

    Added listing resolvers and schema + media files

--- packages/server/src/models/Listing.ts
+++ packages/server/src/models/Listing.ts
@@ -5,10 +5,12 @@ import {
   PrimaryGeneratedColumn,
   ManyToOne
 } from "typeorm";
+import { User } from "./User";
 
 @Entity("listings")
 export class Listing extends BaseEntity {
-  @PrimaryGeneratedColumn("uuid") id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
   @Column("varchar", { length: 100 })
   name: string;
@@ -16,17 +18,27 @@ export class Listing extends BaseEntity {
   @Column("varchar", { length: 255 })
   description: string;
 
-  @Column("text") pictureUrl: string;
+  @Column("text")
+  pictureUrl: string;
 
-  @Column("int") price: number;
+  @Column("varchar", { length: 100 })
+  category: string;
 
-  @Column("double precision") latitude: number;
+  @Column("int")
+  price: number;
 
-  @Column("double precision") longitude: number;
+  @Column("double precision")
+  latitude: number;
+
+  @Column("double precision")
+  longitude: number;
 
   @Column("text", { array: true })
   amenities: string[];
 
-  @ManyToOne(type => User, user => user.photos)
+  @Column("uuid")
+  userId: string;
+
+  @ManyToOne(() => User, user => user.listings)
   user: User;
 }
