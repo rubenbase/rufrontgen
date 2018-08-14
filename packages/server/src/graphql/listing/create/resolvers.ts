735ab57a7c2dbf8f79c5fa515921b3d58a33019d
rubenbase <daggosso@hotmail.com>
2018-08-14 12:31:48 +0200

    Created system for uploading pics

--- packages/server/src/graphql/listing/create/resolvers.ts
+++ packages/server/src/graphql/listing/create/resolvers.ts
@@ -1,18 +1,37 @@
+import * as shortid from "shortid";
+import { createWriteStream } from "fs";
+
 import { ResolverMap } from "../../../types/graphql-utils";
 import { Listing } from "../../../models/Listing";
 
+// Route where images will be saved
+const uploadDir = "images";
+
+const storeUpload = async ({ stream, filename }: any): Promise<any> => {
+  const id = shortid.generate();
+  const path = `${uploadDir}/${id}-${filename}`;
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(createWriteStream(path))
+      .on("finish", () => resolve({ id, path }))
+      .on("error", reject)
+  );
+};
+
+const processUpload = async (upload: any) => {
+  const { stream, filename } = await upload;
+  const { id } = await storeUpload({ stream, filename });
+  return id;
+};
+
 export const resolvers: ResolverMap = {
   Mutation: {
-    createListing: async (_, { input }, { session }) => {
-      console.log(session);
-
-      if (!session.userId) {
-        throw new Error("not authenticated");
-      }
-
+    createListing: async (_, { input: { picture, ...data } }, { session }) => {
+      const pictureUrl = await processUpload(picture);
       await Listing.create({
-        ...input,
-        pictureUrl: "",
+        ...data,
+        pictureUrl,
         userId: session.userId
       }).save();
 
