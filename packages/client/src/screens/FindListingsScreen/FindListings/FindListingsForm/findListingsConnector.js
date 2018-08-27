fb52cdccc598bb1b38465c851b2148b386fbef3a
Marcos Muñiz García <esechinako@gmail.com>
2018-08-27 12:22:36 +0200

    styles listings view

--- packages/client/src/screens/FindListingsScreen/FindListings/FindListingsForm/findListingsConnector.js
+++ packages/client/src/screens/FindListingsScreen/FindListings/FindListingsForm/findListingsConnector.js
@@ -1,8 +1,22 @@
 import * as React from "react";
-import { Card } from "antd";
+import { Card, List, Avatar } from "antd";
 import { withFindListings } from "@rufrontgen/controller";
 
 const { Meta } = Card;
+const data = [
+  {
+    title: "Ant Design Title 1"
+  },
+  {
+    title: "Ant Design Title 2"
+  },
+  {
+    title: "Ant Design Title 3"
+  },
+  {
+    title: "Ant Design Title 4"
+  }
+];
 
 class C extends React.PureComponent {
   render() {
@@ -11,14 +25,18 @@ class C extends React.PureComponent {
       <div>
         {loading && <div>...loading</div>}
         {listings.map(l => (
-          <Card
-            key={`${l.id}-card`}
-            hoverable={true}
-            style={{ width: 240 }}
-            cover={l.pictureUrl && <img alt={l.name} src={l.pictureUrl} />}
-          >
-            <Meta title={l.name} description={l.owner.email} />>
-          </Card>
+          <List
+            itemLayout="horizontal"
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar alt={l.name} src={l.pictureUrl} />}
+                  title={l.name}
+                  description={l.owner.email}
+                />
+              </List.Item>
+            )}
+          />
         ))}
       </div>
     );
