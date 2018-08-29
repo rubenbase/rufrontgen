d91a538c9d091cf83820534d5c9b500a2754252a
Marcos Muñiz García <esechinako@gmail.com>
2018-08-30 00:17:33 +0200

    style create listings

--- packages/client/src/screens/CreateListingScreen/CreateListing/CreateListingForm/ui/page1.js
+++ packages/client/src/screens/CreateListingScreen/CreateListing/CreateListingForm/ui/page1.js
@@ -2,16 +2,29 @@ import * as React from "react";
 import { Field } from "formik";
 import { InputField } from "modules/shared/inputField";
 import { DropzoneField } from "modules/shared/DropzoneField";
+import "./style.scss";
 
 export const Page1 = () => (
   <React.Fragment>
-    <Field name="name" placeholder="Name" component={InputField} />
-    <Field name="category" placeholder="Category" component={InputField} />
     <Field
-      name="description"
-      placeholder="Description"
+      label="Nombre"
+      name="name"
+      placeholder="Escribe el nombre del concurso."
       component={InputField}
     />
+    <Field
+      label="Categoría"
+      name="category"
+      placeholder="Escribe una categoría."
+      component={InputField}
+    />
+    <Field
+      label="Descripción"
+      name="description"
+      placeholder="Escribe una descripción."
+      component={InputField}
+    />
+    <div>Adjunta una imágen:</div>
     <Field name="picture" component={DropzoneField} />
   </React.Fragment>
 );
