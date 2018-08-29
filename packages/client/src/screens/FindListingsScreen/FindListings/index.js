f1ccbb8b8652b7f2ff534c69347aaa5363b52e59
Marcos Muñiz García <esechinako@gmail.com>
2018-08-30 00:23:22 +0200

    add main menu to listings

--- packages/client/src/screens/FindListingsScreen/FindListings/index.js
+++ packages/client/src/screens/FindListingsScreen/FindListings/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Button } from "antd";
 import AuthHeader from "modules/LayoutComponents/AuthHeader/index";
 import { FindListingsConnector } from "./FindListingsForm/findListingsConnector";
+import MainMenu from "modules/LayoutComponents/MainMenu/index";
 class FindListings extends React.Component {
   render() {
     return (
       <div className="login login--fullscreen">
-        <AuthHeader />
+        <MainMenu />
         <div className="login__block mb-5">
           <div className="row">
             <div className="col-xl-12">
