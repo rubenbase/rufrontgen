7e3bbc0e0a8377e6ce057628560b9aaff37aae8f
rubenbase <daggosso@hotmail.com>
2018-09-17 16:05:45 +0200

    Changed siteconfig to support search and internationalization

--- packages/docs/website/siteConfig.js
+++ packages/docs/website/siteConfig.js
@@ -1,100 +1,98 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    caption: "User1",
+    image: "/test-site/img/docusaurus.svg",
+    infoLink: "https://www.example.com",
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: "Rufrontgen",
+  tagline: "Generate websites and mobile apps!",
+  url: "https://docusaurus.io",
+  baseUrl: "/",
+  // For github.io type URLS, you would combine the URL and baseUrl like:
+  // url: 'https://reasonml.github.io',
+  // baseUrl: '/reason-react/',
+  defaultVersionShown: "1.0.0",
+  organizationName: "rubenbase",
+  projectName: "rufrontgen",
+  noIndex: false,
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ["https://buttons.github.io/buttons.js"],
 
-  // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
+  // On page navigation for the current documentation page.
+  onPageNav: "separate",
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    { doc: "doc1", label: "Docs" },
+    { doc: "doc4", label: "API" },
+    { page: "help", label: "Help" },
+    { search: true },
+    { blog: true, label: "Blog" }
   ],
 
-  // If you have users set above, you add it here:
-  users,
-
-  /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
-  favicon: 'img/favicon.png',
-
-  /* Colors for website */
+  headerIcon: "img/docusaurus.svg",
+  favicon: "img/favicon.png",
   colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
+    primaryColor: "#9e42f4",
+    secondaryColor: "#205C3B"
+  },
+  editUrl: "https://github.com/rubenbase/rufrontgen/edit/master/docs/",
+  // Users variable set above
+  users,
+  disableHeaderTitle: false,
+  disableTitleTagline: false,
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: "default"
+  },
+  separateCss: ["static/css/non-docusaurus", "static/assets/separate-css"],
+  footerIcon: "img/docusaurus.svg",
+  translationRecruitingLink: "https://crowdin.com/project/docusaurus",
+  algolia: {
+    apiKey: "0f9f28b9ab9efae89810921a351753b5",
+    indexName: "github"
   },
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
+  markdownPlugins: [
+    function foo(md) {
+      md.renderer.rules.fence_custom.foo = function(
+        tokens,
+        idx,
+        options,
+        env,
+        instance
+      ) {
+        return '<div class="foo">bar</div>';
+      };
+    }
+  ],
+  scripts: [
+    "https://docusaurus.io/slash.js",
+    {
+      src:
+        "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+      async: true
+    }
+  ],
+  stylesheets: [
+    "https://docusaurus.io/style.css",
+    {
+      href: "http://css.link",
+      type: "text/css"
+    }
+  ],
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
-
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
-
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
+  ogImage: "img/docusaurus.png",
   cleanUrl: true,
-
-  // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  scrollToTop: true,
+  scrollToTopOptions: {
+    zIndex: 100
+  }
 };
 
 module.exports = siteConfig;
