c7250f130d82287b93a967c02db4dadd6dc1a760
rubenbase <daggosso@hotmail.com>
2018-09-19 11:14:43 +0200

    Fixed non null graphql errors

--- packages/server/src/models/Listing.ts
+++ packages/server/src/models/Listing.ts
@@ -18,7 +18,7 @@ export class Listing extends BaseEntity {
   @Column("varchar", { length: 255 })
   description: string;
 
-  @Column("text")
+  @Column("text", { nullable: true })
   pictureUrl: string;
 
   @Column("varchar", { length: 100 })
