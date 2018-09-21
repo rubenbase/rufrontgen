abea86a456bbc558bf4ed068450cf88614303147
rubenbase <daggosso@hotmail.com>
2018-09-21 10:25:59 +0200

    Added prettier-parse theme

---
+++ docs/editors.md
@@ -0,0 +1,41 @@
+---
+id: editors
+title: Editor Integration
+---
+
+## Atom
+
+Atom users can simply install the [prettier-atom] package and use `Ctrl+Alt+F` to format a file (or format on save if enabled).
+
+Alternatively, you can use one the packages below, which behave similarly to [prettier-atom] but have a focus on minimalism.
+
+- [mprettier](https://github.com/t9md/atom-mprettier)
+- [miniprettier](https://github.com/duailibe/atom-miniprettier)
+
+## Emacs
+
+Emacs users should see [this repository](https://github.com/prettier/prettier-emacs) for on-demand formatting.
+
+## Vim
+
+Vim users can simply install either [sbdchd](https://github.com/sbdchd)/[neoformat](https://github.com/sbdchd/neoformat), [w0rp](https://github.com/w0rp)/[ale](https://github.com/w0rp/ale), or [prettier](https://github.com/prettier)/[vim-prettier](https://github.com/prettier/vim-prettier), for more details see [the vim setup guide](vim.md).
+
+## Visual Studio Code
+
+`prettier-vscode` can be installed using the extension sidebar. Search for `Prettier - Code formatter`. It can also be installed using `ext install prettier-vscode` in the command palette. [Check its repository for configuration and shortcuts](https://github.com/prettier/prettier-vscode).
+
+If you'd like to toggle the formatter on and off, install [`vscode-status-bar-format-toggle`](https://marketplace.visualstudio.com/items?itemName=tombonnike.vscode-status-bar-format-toggle).
+
+## Visual Studio
+
+Install the [JavaScript Prettier extension](https://github.com/madskristensen/JavaScriptPrettier).
+
+## Sublime Text
+
+Sublime Text support is available through Package Control and the [JsPrettier](https://packagecontrol.io/packages/JsPrettier) plug-in.
+
+## JetBrains WebStorm, PHPStorm, PyCharm...
+
+See the [WebStorm setup guide](webstorm.md).
+
+[prettier-atom]: https://github.com/prettier/prettier-atom
--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,101 +1,70 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+"use strict";
 
-const React = require('react');
+const React = require("react");
+const PropTypes = require("prop-types");
+
+const GithubButton = props => (
+  <a
+    className="github-button"
+    href={props.config.githubUrl}
+    data-icon="octicon-star"
+    data-show-count="true"
+    aria-label="Star this project on GitHub"
+  >
+    Star
+  </a>
+);
+
+GithubButton.propTypes = {
+  config: PropTypes.object
+};
 
 class Footer extends React.Component {
-  docUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return `${baseUrl}docs/${language ? `${language}/` : ''}${doc}`;
+  url(path) {
+    const language = this.props.language || "en";
+    return `${this.props.config.baseUrl}docs/${language}${path}`;
   }
 
-  pageUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+  usersUrl() {
+    const language = this.props.language || "en";
+    return `${this.props.config.baseUrl}${language}/users`;
   }
 
   render() {
     return (
-      <footer className="nav-footer" id="footer">
+      <footer className="footerSection nav-footer" id="footer">
         <section className="sitemap">
           <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
+            <img
+              src={this.props.config.baseUrl + this.props.config.footerIcon}
+              alt={this.props.config.title}
+            />
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
-            </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
+            <a href={this.url("/index.html")}>About</a>
+            <a href={this.url("/install.html")}>Usage</a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
-            </a>
+            <a href={this.usersUrl()}>User Showcase</a>
           </div>
           <div>
             <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
+            <a href={this.props.config.baseUrl + "blog"}>Blog</a>
+            <a href={this.props.config.githubUrl}>GitHub</a>
+            <a href={this.props.config.githubUrl + "/issues"}>Issues</a>
+            <GithubButton config={this.props.config} />
           </div>
         </section>
-
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
-        <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
   }
 }
+
+Footer.propTypes = {
+  language: PropTypes.string,
+  config: PropTypes.object
+};
 
 module.exports = Footer;
