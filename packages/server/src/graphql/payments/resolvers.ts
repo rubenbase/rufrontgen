d3f34ee639f783de942c7a9fdf8788cb0254b3ee
rubenbase <daggosso@hotmail.com>
2018-10-07 16:10:47 +0200

    Added environment variables for payments + fix stripe subscription

--- packages/server/src/graphql/payments/resolvers.ts
+++ packages/server/src/graphql/payments/resolvers.ts
@@ -1,7 +1,7 @@
+import { stripe } from "./../../utils/payments/stripe";
 import { User } from "./../../models/User";
 import { ResolverMap } from "../../types/graphql-utils";
 import { getConnection } from "typeorm";
-// import { Billing } from "../../models/Billing";
 
 export const resolvers: ResolverMap = {
   Mutation: {
@@ -10,38 +10,30 @@ export const resolvers: ResolverMap = {
         throw new Error("not authenticated");
       }
 
-      const user = await User.findOne("43a64a1b-d5d9-4373-aedd-25d3c0aba985");
-      console.log("ALIBABA USER IS ->", user);
-      console.log(source);
-
-      const user2 = await getConnection()
+      const user = await getConnection()
         .getRepository(User)
         .createQueryBuilder("user")
         .innerJoinAndSelect("user.billing", "billing")
-        // .whereInIds("43a64a1b-d5d9-4373-aedd-25d3c0aba985")
         .where("user.id = :id", {
-          id: "c0b4440c-c0c2-4f11-a68f-e1c26f4d09ab"
+          id: `${session.userId}`
         })
-
-        // .where("id", { id: "43a64a1b-d5d9-4373-aedd-25d3c0aba985" })
         .getOne();
 
-      console.log("ALIBABA 22222222222222222 -> ", user2);
-      // if (!user) {
-      //   throw new Error();
-      // }
-      // console.log(source);
-      // const customer = await stripe.customers.create({
-      //   email: user.email,
-      //   source,
-      //   plan: process.env.PLAN
-      // });
+      if (!user) {
+        throw new Error();
+      }
 
-      // user.stripeId = customer.id;
-      // user.type = "paid";
-      // await user.save();
+      const customer = await stripe.customers.create({
+        email: user.email,
+        source,
+        plan: process.env.STRIPE_PLANID
+      });
 
-      return true;
+      user.billing.stripeId = customer.id;
+      user.billing.plan = "paid";
+      await user.save();
+
+      return user;
     }
   }
 };
