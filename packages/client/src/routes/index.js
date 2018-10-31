3068c88c0f8010c0e7636ba13008fc9d42b16c02
rubenbase <daggosso@hotmail.com>
2018-10-31 11:23:26 +0100

    Finished route based code splitting, added chunk naming to control code splitting and added tempLoading.

--- packages/client/src/routes/index.js
+++ packages/client/src/routes/index.js
@@ -91,68 +91,55 @@ const Text = Loadable({
   loading: () => tmp_LoadingComponent
 });
 
-const loadableRoutes = {
-  /* Category Routes */
-  // "/create-categories": {
-  //   component: loadable(() => import("screens/CreateCategoryScreen"))
-  // },
-  // "/categories": {
-  //   component: loadable(() => import("screens/FindCategoryScreen"))
-  // },
-  // "/view-categories": {
-  //   component: loadable(() => import("screens/ViewCategoryScreen"))
-  // },
-  // "/edit-categories": {
-  //   component: loadable(() => import("screens/EditCategoryScreen"))
-  // },
-
-  /* Legal Routes */
-  // "/legal": {
-  //   component: loadable(() => import("containers/legal/LegalConnector"))
-  // },
-
-  /* Misc Routes */
-  "/m/": {
-    component: loadable(() => import("screens/TextScreen")),
-    exact: false
-  },
-
-  /* In maintenance Routes */
-  "/home": {
-    component: loadable(() => import("screens/HomeScreen"))
-  }
-};
+/****************** In Maintenance Routes ******************/
+const Home2 = Loadable({
+  loader: () =>
+    import("screens/HomeScreen" /* webpackChunkName: "Home2Chunk" */),
+  loading: () => tmp_LoadingComponent
+});
 
 class Routes extends React.PureComponent {
-  timeoutId = null;
-
-  componentDidMount() {
-    this.timeoutId = setTimeout(
-      () =>
-        Object.keys(loadableRoutes).forEach(path =>
-          loadableRoutes[path].component.preload()
-        ),
-      2000 // load after 5 sec
-    );
-  }
-
-  componentWillUnmount() {
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-  }
-
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomeView} />
-          {Object.keys(loadableRoutes).map(path => {
-            const { exact, ...props } = loadableRoutes[path];
-            props.exact = exact === void 0 || exact || false; // set true as default
-            console.log(props.exact);
-            return <Route key={path} path={path} {...props} />;
-          })}
+          <Route exact={true} path="/home" component={Home2} />
+
+          {/* Auth Flow Routes */}
+          <Route exact={true} path="/register" component={Register} />
+          <Route exact={true} path="/login" component={Login} />
+          <Route exact={true} path="/logout" component={Logout} />
+          <Route
+            exact={true}
+            path="/forgot-password"
+            component={ForgotPassword}
+          />
+          <Route
+            exact={true}
+            path="/change-password/:key"
+            component={ChangePassword}
+          />
+
+          {/* Listings Routes */}
+          <Route exact={true} path="/listings" component={Listings} />
+          <Route exact={true} path="/listing/:listingId" component={Listing} />
+          <AuthRoute
+            exact={true}
+            path="/listing/:listingId/edit"
+            component={ListingEdit}
+          />
+          <AuthRoute
+            exact={true}
+            path="/create-listing"
+            component={ListingCreate}
+          />
+
+          {/* Legal Routes */}
+          <Route exact={true} path="/legal" component={Legal} />
+
+          {/*Misc Routes */}
+          <Route path="/m/" component={Text} />
           <Route
             render={() => (
               <Page>
@@ -166,95 +153,4 @@ class Routes extends React.PureComponent {
   }
 }
 
-export { loadableRoutes };
 export { Routes };
-
-// export const Routes = () => (
-//   <BrowserRouter>
-//     <Switch>
-{
-  /* <Route exact={true} path="/" component={HomeView} /> */
-}
-{
-  /* <Route exact={true} path="/home" component={Home} /> */
-}
-
-{
-  /* Auth Flow Routes */
-}
-{
-  /* <Route exact={true} path="/register" component={RegisterScreen} />
-      <Route exact={true} path="/login" component={LoginScreen} />
-      <Route exact={true} path="/logout" component={Logout} />
-      <Route
-        exact={true}
-        path="/forgot-password"
-        component={ForgotPasswordScreen}
-      />
-      <Route
-        exact={true}
-        path="/change-password/:key"
-        component={ChangePasswordScreen}
-      /> */
-}
-
-{
-  /* Listings Routes */
-}
-{
-  /* <Route exact={true} path="/listings" component={FindListingsScreen} />
-      <Route
-        exact={true}
-        path="/listing/:listingId"
-        component={ViewListingScreen}
-      />
-      <AuthRoute
-        exact={true}
-        path="/listing/:listingId/edit"
-        component={EditListingScreen}
-      />
-
-      <AuthRoute
-        exact={true}
-        path="/create-listing"
-        component={CreateListingScreen}
-      /> */
-}
-
-{
-  /* Category Routes */
-}
-{
-  /* <Route
-        exact={true}
-        path="/create-categories"
-        component={CreateCategoryScreen}
-      />
-
-      <Route exact={true} path="/categories" component={FindCategoryScreen} />
-
-      <Route
-        exact={true}
-        path="/view-categories"
-        component={ViewCategoryScreen}
-      />
-
-      <Route
-        exact={true}
-        path="/edit-categories"
-        component={EditCategoryScreen}
-      /> */
-}
-
-{
-  /* Legal Routes */
-}
-// <Route exact={true} path="/legal" component={LegalConnector} />
-
-{
-  /* Misc Routes */
-}
-// <Route path="/m/" component={TextScreen} />
-//     </Switch>
-//   </BrowserRouter>
-// );
